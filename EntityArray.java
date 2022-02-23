139736f31a4597e4e847b6564a9c5575e95f1712
Сергей <100167829+SergeySytsevich@users.noreply.github.com>
2022-02-23 17:35:41 +0100

    Add files via upload
    Это я очень не хотел показывать, но тут есть вопрос .....

---
+++ EntityArray.java
@@ -0,0 +1,13 @@
+
+package com.epam.task1.entity;
+import java.util.ArrayList;
+
+public class EntityArray extends ArrayList{
+    public EntityArray getArray(){
+        return this;
+    }
+    
+    public EntityArray(){
+        super();
+    }
+}
